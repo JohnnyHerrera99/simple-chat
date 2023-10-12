19333ea8d509c68e7346e0ca0d50f771a644c971
Johnny Herrera <jherrera@tastymeta.com>
2023-10-12 13:38:44 -0400

    added test cases

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class RegistrationTestCase(TestCase):
+
+    def test_registration_form_valid(self):
+        form_data = {
+            'username': 'john1234',
+            'first_name': 'John',
+            'last_name': 'Lennon',
+            'email': 'jhon4@beatles.com',
+            'password1': '1mag1n3a',
+            'password2': '1mag1n3a',
+        }
+        response = self.client.post(reverse('register'), data=form_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/")
+        self.assertTrue(User.objects.filter(username=form_data["username"]).exists())
+
+    def test_registration_form_invalid(self):
+        form_data = {
+            'username': '',
+            'first_name': 'John',
+            'last_name': 'Lennon',
+            'email': 'jhon4@beatles.com',
+            'password1': '1mag1n3a',
+            'password2': '1mag1n3allp30pl3',
+        }
+
+        response = self.client.post(reverse('register'), data=form_data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'form', 'username', 'This field is required.')
+        self.assertFormError(response, 'form', 'password2', 'The two password fields didn’t match.')
+        self.assertFalse(User.objects.filter(email='jhon4@beatles.com').exists())
--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Message
 
-# Create your tests here.
+User = get_user_model()
+USER_DATA_TEST = dict(
+    username='john123',
+    email='jhon4@beatles.com',
+    password='1mag1n3',
+    first_name='john',
+    last_name='lennon'
+)
+
+
+class MessageTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(
+            **USER_DATA_TEST
+        )
+    
+    def test_message_created(self):
+        user = User.objects.get(username=USER_DATA_TEST["username"])
+        obj = Message.objects.create(
+            username=user.username,
+            content="Imagine all the people. Livin' for today",
+            user=user,
+            room="the-70"
+        )
+        self.assertIsNotNone(obj)
+
+
+class TestCalls(TestCase):
+    login_path = '/accounts/login/?redirect_to='
+
+    def setUp(self):
+        User.objects.create_user(
+            **USER_DATA_TEST
+        )
+
+    def test_call_view_deny_anonymous_homepage(self):
+        url_path = "/"
+        response = self.client.get(url_path, follow=True)
+        self.assertRedirects(response, self.login_path + url_path)
+        response = self.client.post(url_path, follow=True)
+        self.assertRedirects(response, self.login_path + url_path)
+
+    def test_call_view_load_homepage(self):
+        self.client.login(username=USER_DATA_TEST["username"], password=USER_DATA_TEST["password"])
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'chat/index.html')
+
+    def test_call_view_deny_anonymous_room(self):
+        url_path = "/room/test/"
+        response = self.client.get(url_path, follow=True)
+        self.assertRedirects(response, self.login_path + url_path)
+        response = self.client.post(url_path, follow=True)
+        self.assertRedirects(response, self.login_path + url_path)
+
+    def test_call_view_load_room(self):
+        self.client.login(username=USER_DATA_TEST["username"], password=USER_DATA_TEST["password"])
+        response = self.client.get('/room/test/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'chat/room.html')
