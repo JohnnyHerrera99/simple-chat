0a1c125d9d8925fb1afbc81829c4da9e3f3373a5
Johnny Herrera <jherrera@tastymeta.com>
2023-10-12 13:37:00 -0400

    update url for register view

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("accounts/register/", views.register_request, name="register")
+    path("accounts/register/", views.RegisterFormView.as_view(), name="register")
 ]
