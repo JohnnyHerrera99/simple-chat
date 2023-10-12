18bc9ef616666be114cb3c2a468fff98b972b078
Johnny Herrera <jherrera@tastymeta.com>
2023-10-12 13:37:27 -0400

    update forms for register and message

--- account/forms.py
+++ account/forms.py
@@ -5,10 +5,12 @@ from django.contrib.auth.models import User
 
 class NewUserForm(UserCreationForm):
 	email = forms.EmailField(required=True)
+	first_name = forms.CharField(required=True)
+	last_name = forms.CharField(required=True)
 
 	class Meta:
 		model = User
-		fields = ("username", "email", "password1", "password2")
+		fields = ("username", "first_name", "last_name", "email", "password1", "password2")
 
 	def save(self, commit=True):
 		user = super(NewUserForm, self).save(commit=False)
