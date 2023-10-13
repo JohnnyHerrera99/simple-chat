27fc03eaba7fc89f7ed836b2970e4ac5eea0ceb9
Johnny Herrera <jherrera@tastymeta.com>
2023-10-13 10:45:24 -0400

    add loentry admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,26 @@
+from django.contrib.admin.models import LogEntry
 from django.contrib import admin
 from . import models
 
 
 admin.site.register(models.Message)
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'action_time'
+
+    list_filter = [
+        'user',
+        'content_type',
+        'action_flag'
+    ]
+    search_fields = [
+        'object_repr',
+        'change_message'
+    ]
+    list_display = [
+        'action_time',
+        'user',
+        'content_type',
+        'action_flag',
+    ]
